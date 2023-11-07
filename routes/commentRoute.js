68c1ad498cda4613194e2d2e704886ba6255ccc4
n1steka <mongolzkarigun@gmail.com>
2023-11-07 10:57:45 +0800

    comment bichxed user id awdag blsn

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,19 +1,19 @@
 express = require("express");
 const upload = require("../middleware/fileUpload");
-
+const { protect } = require("../middleware/protect");
 const {
   create,
   update,
   detail,
   findDelete,
   getAll,
-  getPostComments
+  getPostComments,
 } = require("../controller/commentController");
 const router = express.Router();
 
 // upload.single("file"),
 
-router.route("/").post(upload.single("file"), create).get(getAll);
+router.route("/").post(protect, upload.single("file"), create).get(getAll);
 router.route("/:id").put(update).delete(findDelete).get(detail);
 //"/api/v1/moktaText"
 module.exports = router;
